4b047badf6574331ed68e91421b58c6f9018a6c1
Prasanna Vijayan <prasannavijayan446@gmail.com>
2019-02-09 03:51:42 +0530

    Crud game

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,9 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
 
+	response  : undefined,
+	editData  : undefined,
+
 	actions: {
 		createGame() {
 			let title = this.get('title');
@@ -9,6 +12,40 @@ export default Controller.extend({
 			let newGame = this.store.createRecord('game', { title })
 
 			newGame.save();
+		},
+
+		editRecord( data ) {
+			this.set( 'editData', data );
+
+			new Promise( respond => {
+				this.addObserver( 'response', function observer(){
+          this.removeObserver( 'response', observer );
+          respond( this.get( 'response' ) );
+				});
+			}).then(proceed => {
+				const props = {
+					editData: undefined,
+					response: false
+				}
+
+				if (proceed) {
+					let data = this.get('editData');
+					this.store.findRecord('game', data.id).then( data => {
+						data.save();
+					})
+				}
+
+				this.setProperties(props);
+			});
+		},
+
+		response( response ) {
+			this.set('response', response);
+		},
+
+		deleteGame( data ) {
+			data.deleteRecord();
+			data.save();
 		}
 	}
 });
